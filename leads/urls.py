9ea4dd2816f747e7ebe163117105eadfc51310ea
ronyshabo <ronyshabbo@gmail.com>
2023-05-09 14:38:23 -0500

    adding tailblocks, adding new html templates, and work with view classes

--- leads/urls.py
+++ leads/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import Lead_list,secondpage,lead_detail,lead_create,lead_update,lead_delete
+from .views import (Lead_list,secondpage,lead_detail,lead_create,lead_update,
+lead_delete, LeadListView, LeadDetailView)
 
 app_name = "leads"
 
 urlpatterns = [
-    path('',Lead_list,name='lead-list'),
+    path('',LeadListView.as_view(),name='lead-list'),
     # path('2/',secondpage),
-    path('<int:pk>/',lead_detail,name='lead-detail'),
+    path('<int:pk>/',LeadDetailView.as_view(),name='lead-detail'),
     path('<int:pk>/update',lead_update,name='lead-update'),
     path('<int:pk>/delete',lead_delete,name='lead-delete'),
     path('create/',lead_create,name='lead-create'),
--- leads/views.py
+++ leads/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead,Agent
 from .forms import LeadForm,LeadModelForm
+# With the TemplateView class. we can add all 4 CRUD methods to this class. eg: UpdateView
+from django.views.generic import TemplateView, ListView, DetailView
+
 import logging
 
 logging.basicConfig(
@@ -10,6 +13,20 @@ logging.basicConfig(
     format="%(module)s : %(levelname)s:  %(message)s - : %(asctime)s",
 )
 
+
+class LandingPageView(TemplateView):
+    template_name="landing.html"
+    
+
+def landing_page(request):
+     return render( request, 'landing.html')
+
+
+class LeadListView(ListView):
+    template_name = " leads/lead_list.html"
+    queryset = Lead.objects.all()
+    context_object_name = "leads"
+    
 def Lead_list(request):
     leads = Lead.objects.all()
     context = {
@@ -17,9 +34,10 @@ def Lead_list(request):
     }
     return render( request, 'leads/lead_list.html',context)
 
-def secondpage(request):
-
-    return render( request, 'secondpage.html')
+class LeadDetailView(DetailView):
+    template_name = "leads/lead_detail.html"
+    queryset = Lead.objects.all()
+    context_object_name = "lead"
 
 def lead_detail(request,pk):
     lead= Lead.objects.get(id=pk)
@@ -77,3 +95,9 @@ def lead_delete(request,pk):
     lead = Lead.objects.get(id=pk)
     lead.delete()
     return redirect('/leads')
+
+
+
+def secondpage(request):
+
+    return render( request, 'secondpage.html')
