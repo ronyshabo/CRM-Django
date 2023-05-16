0f4cb2fbfac2fba4e77c572478870ced766024e6
ronyshabo <ronyshabbo@gmail.com>
2023-05-16 15:38:14 -0500

    static files, urls, and settings

--- django_crm/urls.py
+++ django_crm/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from leads.views import LandingPageView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', include('leads.urls',namespace="leads")),
     path('',LandingPageView.as_view(),name='landing_page')
     ]
+
---
+++ static/JS/main.js
@@ -0,0 +1,1 @@
+Console.log("Welcome to the js file")
